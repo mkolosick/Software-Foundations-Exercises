e032a18b50495ce54138dd04503189725993c97c
mkolosick <kolosick.m@husky.neu.edu>
2013-12-15 13:39:07 -0500

    Tabs to spaces

--- Basics.v
+++ Basics.v
@@ -1,9 +1,9 @@
 (* Exercise: 1 star (nandb) *)
 Definition nandb (b1 : bool) (b2 : bool) : bool :=
-	match b1 with
-		| false => true
-		| true => negb b2
-	end.
+  match b1 with
+    | false => true
+    | true => negb b2
+  end.
 
 Example test_nandb1: (nandb true false) = true.
 Proof. reflexivity. Qed.
@@ -16,13 +16,13 @@ Proof. reflexivity. Qed.
 
 (* Exercise: 1 star (andb3) *)
 Definition andb3 (b1 : bool) (b2 : bool) (b3 : bool) : bool :=
-	match b1 with
-		| true => match b2 with
-								| true => b3
-								| false => false
-							end
-		| false => false
-	end.
+  match b1 with
+    | true => match b2 with
+                | true => b3
+                | false => false
+              end
+    | false => false
+  end.
 
 Example test_andb31: (andb3 true true true) = true.
 Proof. reflexivity. Qed.
@@ -35,10 +35,10 @@ Proof. reflexivity. Qed.
 
 (* Exercise: 1 star (factorial) *)
 Fixpoint factorial (n : nat) : nat :=
-	match n with
-		| 0 => 1
-		| S n' => (S n') * (factorial n')
-	end.
+  match n with
+    | 0 => 1
+    | S n' => (S n') * (factorial n')
+  end.
 
 Example test_factorial1: (factorial 3) = 6.
 Proof. reflexivity. Qed.
@@ -47,13 +47,13 @@ Proof. reflexivity. Qed.
 
 (* Exercise: 2 star (blt_nat) *)
 Fixpoint ble_nat (n m : nat) : bool :=
-	match n with
-		| 0 =>	true
-		| S n' =>	match m with
-								| 0 => false
-								| S m' => ble_nat n' m'
-							end
-	end.
+  match n with
+    | 0 =>  true
+    | S n' =>  match m with
+                | 0 => false
+                | S m' => ble_nat n' m'
+              end
+  end.
 
 Example test_ble_nat1: (ble_nat 2 2) = true.
 Proof. reflexivity. Qed.
